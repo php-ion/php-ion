57518bdc21f6e74395d0781188726492518fd021
bzick <a.cobest@gmail.com>
2015-09-09 15:38:27 +0300

    Test abbreviation :)

--- tests/cases/IONTest.php
+++ tests/cases/IONTest.php
@@ -4,6 +4,21 @@ use ION\Test\TestCase;
 
 class IONTest extends TestCase {
 
+	public function testAbbr() {
+		$abbrs = [
+			"Input-Output Notifications",
+			"Input, Output, Notifications",
+			"Input-Output, Notifications",
+			"Input, Output Notifications",
+		];
+
+		foreach($abbrs as $abbr) {
+			$this->assertEquals(1, preg_match('/^(I)nput(?:, |-)(O)utput,? (N)otifications$/i', $abbr, $matches));
+			$this->assertEquals("ION", $matches[1].$matches[2].$matches[3]);
+		}
+
+	}
+
     /**
      * @memcheck
      */
