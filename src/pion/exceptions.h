aa3aa491bf146f05a1608ebf4393aa42587ad4a0
bzick <a.cobest@gmail.com>
2015-10-29 12:22:04 +0300

    Cleanup

--- src/pion/exceptions.h
+++ src/pion/exceptions.h
@@ -28,34 +28,9 @@ BEGIN_EXTERN_C()
 // ION basic exceptions
 extern ZEND_API zend_class_entry * ion_ce_ION_RuntimeException;
 extern ZEND_API zend_class_entry * ion_ce_ION_InvalidUsageException;
-//#define ion_ce_ION_RuntimeException           spl_ce_RuntimeException
-//#define ion_ce_ION_InvalidUsageException      spl_ce_LogicException
 
 zend_object * pion_exception_new(zend_class_entry * exception_ce, const char * message, long code);
 zend_object * pion_exception_new_ex(zend_class_entry * exception_ce, long code, const char * message, ...);
-
-//#define pion_throw(ce, message, code) zend_throw_exception(ce,  message, code TSRMLS_CC)
-//#define pion_throw_invalid_argument_exception(message) zend_throw_exception(ion_get_class(ION_InvalidArgumentException),  message, -1 TSRMLS_CC)
-//#define pion_throw_ex(ce, code, format, ...) zend_throw_exception_ex(class_name, code TSRMLS_CC, message, ##__VA_ARGS__)
-
-//#define ion_throw_invalid_argument_exception(message) pion_throw(ion_get_class(ION_InvalidArgumentException), message, -1)
-//
-//#define Throw(class_name, message, code)                                            \
-//    zend_throw_exception(class_name,  message, code TSRMLS_CC);                     \
-//
-//#define ThrowEx(class_name, code, message, ...)                                     \
-//    zend_throw_exception_ex(class_name, code TSRMLS_CC, message, ##__VA_ARGS__);    \
-//
-//#define ThrowRuntime(message, code)     Throw(spl_ce_RuntimeException, message, code)
-//#define ThrowRuntimeEx(code, message, ...)     \
-//    ThrowEx(spl_ce_RuntimeException, code, message, ##__VA_ARGS__)
-//#define ThrowUnsupported(message)       ThrowRuntime(message, 1)
-//#define ThrowLogic(message, code)       Throw(spl_ce_LogicException, message, code)
-//#define ThrowLogicEx(code, message, ...)       \
-//    ThrowEx(spl_ce_LogicException, code, message, ##__VA_ARGS__)
-//#define ThrowInvalidArgument(message)   Throw(spl_ce_InvalidArgumentException, message, 1)
-//#define ThrowInvalidArgumentEx(message, ...)     \
-//    ThrowEx(spl_ce_InvalidArgumentException, 1, message, ##__VA_ARGS__)
 
 END_EXTERN_C()
 
