5f014a352aa63550422410e35d54fed9fb2a3b97
bzick <a.cobest@gmail.com>
2015-12-05 14:46:57 +0300

    Cleanup code

--- src/ION/Listener.c
+++ src/ION/Listener.c
@@ -1,9 +1,7 @@
 #include "../pion.h"
-#include <ext/standard/url.h>
 #include <event2/listener.h>
 #include <sys/un.h>
 #include <event2/bufferevent_ssl.h>
-#include <openssl/ossl_typ.h>
 
 
 zend_object_handlers ion_oh_ION_Listener;
@@ -217,79 +215,6 @@ METHOD_ARGS_BEGIN(ION_Listener, encrypt, 1)
 METHOD_ARGS_END()
 
 
-/** public function ION\Listener::start() : self */
-CLASS_METHOD(ION_Listener, start) {
-    ion_listener * listener = get_this_instance(ion_listener);
-    php_url * resource;
-
-    resource = php_url_parse_ex(listener->name->val, listener->name->len);
-    if(resource->host) { // ipv4, ipv6, hostname
-        struct sockaddr_storage sock;
-        socklen_t sock_len = sizeof(struct sockaddr_storage);
-        int result = evutil_parse_sockaddr_port(resource->host, (struct sockaddr *)&sock, (int *)&sock_len);
-        if(result == SUCCESS) {
-            if(sock.ss_family == AF_INET) {
-                listener->flags |= ION_STREAM_NAME_IPV4;
-                struct sockaddr_in * sin = (struct sockaddr_in *) &sock;
-                sin->sin_port = ntohs(resource->port);
-            } else if(sock.ss_family == AF_INET6) {
-                listener->flags |= ION_STREAM_NAME_IPV6;
-                struct sockaddr_in6 * sin6 = (struct sockaddr_in6 *) &sock;
-                sin6->sin6_port = ntohs(resource->port);
-            } else {
-                zend_throw_exception_ex(ion_class_entry(ION_InvalidUsageException), 0, "Address family %d not supported by protocol family", sock.ss_family);
-                return;
-            }
-        } else {
-            zend_throw_exception(ion_class_entry(InvalidArgumentException), "Address is not well-formed", 0);
-            return;
-        }
-        listener->listener = evconnlistener_new_bind(GION(base), _ion_listener_accept, listener,
-                                                     LEV_OPT_CLOSE_ON_FREE | LEV_OPT_REUSEABLE | LEV_OPT_CLOSE_ON_EXEC, -1,
-                                                     (struct sockaddr *)&sock, sock_len);
-        if(listener->listener) {
-            zend_string_release(listener->name);
-            listener->name = NULL;
-            pion_net_sock_name(evconnlistener_get_fd(listener->listener), PION_NET_NAME_LOCAL, &listener->name);
-        }
-    } else if(resource->path) { // unix socket
-        evutil_socket_t fd;
-        struct sockaddr_un local;
-        socklen_t sock_len = sizeof(struct sockaddr_un);
-
-        listener->flags |= ION_STREAM_NAME_UNIX;
-        fd = socket(AF_UNIX, SOCK_STREAM, 0);
-        local.sun_family = AF_UNIX;
-        strncpy(local.sun_path, resource->path, 100);
-        unlink(local.sun_path);
-        if(bind(fd, (struct sockaddr *)&local, sock_len) == FAILURE) {
-            zend_throw_exception_ex(ion_class_entry(ION_RuntimeException), errno, "Failed to listen on %s: %s", listener->name->val, evutil_socket_error_to_string(errno));
-            return;
-        }
-        listener->listener = evconnlistener_new(GION(base), _ion_listener_accept, listener,
-                                                LEV_OPT_CLOSE_ON_FREE | LEV_OPT_REUSEABLE | LEV_OPT_CLOSE_ON_EXEC, -1, fd);
-        zend_string_release(listener->name);
-        listener->name = zend_string_init(resource->path, strlen(resource->path) + 1, 0);
-    } else {
-        zend_throw_exception(ion_class_entry(ION_InvalidUsageException), "Invalid socket name", 0);
-        return;
-    }
-    php_url_free(resource);
-
-    if(listener->listener) {
-        evconnlistener_set_error_cb(listener->listener, _ion_listener_error);
-        evconnlistener_enable(listener->listener);
-        listener->flags |= ION_STREAM_STATE_ENABLED;
-    } else {
-        zend_throw_exception_ex(ion_class_entry(ION_RuntimeException), errno, "Failed to listen on %s: %s", listener->name->val, evutil_socket_error_to_string(errno));
-        return;
-    }
-
-    RETURN_THIS();
-}
-
-METHOD_WITHOUT_ARGS(ION_Listener, start);
-
 /** public function ION\Listener::enable() : self */
 CLASS_METHOD(ION_Listener, enable) {
     ion_listener * listener = get_this_instance(ion_listener);
@@ -342,7 +267,6 @@ CLASS_METHODS_START(ION_Listener)
     METHOD(ION_Listener, __construct,   ZEND_ACC_PUBLIC)
     METHOD(ION_Listener, encrypt,       ZEND_ACC_PUBLIC)
     METHOD(ION_Listener, accept,        ZEND_ACC_PUBLIC)
-    METHOD(ION_Listener, start,         ZEND_ACC_PUBLIC)
     METHOD(ION_Listener, enable,        ZEND_ACC_PUBLIC)
     METHOD(ION_Listener, disable,       ZEND_ACC_PUBLIC)
     METHOD(ION_Listener, shutdown,      ZEND_ACC_PUBLIC)
